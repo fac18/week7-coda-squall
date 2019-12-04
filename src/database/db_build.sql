0e58fc2b0ec022d77b44c4622ed59b42133a7445
renatajarmova <renatajarmova@gmail.com>
2019-12-04 17:28:28 +0000

    Add initial db_build.sql file content
    Co-authored-by: Renata <renatajarmova@gmail.com>
    Co-authored-by: Dan <dangoss@posteo.net>
    Co-authored-by: Roshan <roshlarosh@gmail.com>
    
    Co-authored-by: Gillian <gillian@yeomans.org.uk>

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,1 +1,23 @@
-hi
+BEGIN;
+
+DROP TABLE IF EXISTS characters CASCADE;
+DROP TABLE IF EXISTS powers CASCADE;
+
+CREATE TABLE characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    talisman VARCHAR(30) NOT NULL,
+    battle_cry VARCHAR(200) NOT NULL,
+);
+
+CREATE TABLE powers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    description TEXT NOT NULL,
+    image VARBINARY(MAX)
+);
+
+INSERT INTO powers (name, description, image) VALUES 
+('Electricity', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB))
+
+COMMIT;
