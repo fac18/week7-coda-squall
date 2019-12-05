2b82eea5d2669a7f79cd7a78e8251b5dcd5b0d36
plasticneuron <plasticneuron@posteo.net>
2019-12-05 17:35:16 +0000

    tidy up main.js file and grab submit buttons in preparation for dynamic ajax requests

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
+const postButton = document.querySelector('.char-form__submit')
+const getButton = document.querySelector('.return-form__submit')
+
+// define DOM injection function for populating leaderboard with all characters
+const populateAllChar = res => {
+  console.log(res);
+};
+
+// define function for populating .player-section with character info
+const populatePlayer = char => {
+
+}
+
+// populate leaderboard with all characters on loading of page
 window.onload = () => {
   backendCall("/get-all-char", "GET", null, res => {
     populateAllChar(res);
   });
-};
-
-const populateAllChar = res => {
-  console.log(res);
 };
