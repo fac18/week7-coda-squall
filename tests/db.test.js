72346feaa3c86e041fe6d936c33a12cd3c931953
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-05 12:13:01 +0000

    add first tests

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,1 +1,23 @@
-hi;
+const tape = require("tape");
+const runDbBuild = require("../src/database/db_build");
+const getData = require("../src/queries/getData");
+const postData = require("../src/queries/postData");
+
+tape("Tape is working", t => {
+  t.equal(1, 1, "one is equal to one");
+  t.end();
+});
+
+tape("Get all powers", t => {
+  runDbBuild((err, res) => {
+    if (err) console.log(err);
+    return;
+  });
+  let actual = getData.getPow((err, res) => {
+    if (err) return err;
+    return res;
+  });
+  let expected = "";
+  t.deepEqual(actual, expected, "These are all the powers : ");
+  t.end();
+});
