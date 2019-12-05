b704f283dacfea2b7b0d48676a09fb0fa9330220
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-05 11:40:05 +0000

    create getChar, getAllChar and getPow

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,1 +1,31 @@
-hi;
+const dbConnection = require("../database/db_connection");
+
+const getAllChar = cb => {
+  dbConnection.query("SELECT * FROM characters", (err, result) => {
+    if (err) return cb(err);
+    cb(null, result.rows);
+  });
+};
+
+const getChar = (name, cb) => {
+  dbConnection.query(
+    `SELECT * FROM characters WHERE name = ${name}`,
+    (err, result) => {
+      if (err) return cb(err);
+      cb(null, result.rows);
+    }
+  );
+};
+
+const getPow = cb => {
+  dbConnection.query("SELECT * FROM powers", (err, result) => {
+    if (err) return cb(err);
+    cb(null, result.rows);
+  });
+};
+
+module.exports = {
+  getAllChar,
+  getChar,
+  getPow
+};
