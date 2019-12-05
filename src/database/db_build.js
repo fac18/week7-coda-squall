f7995c5d36ee3c59861b227f94786ee33545b50e
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-05 12:12:21 +0000

    add runDbBuild to dbConnection query

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const dbConnection = require("./db_connection");
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-dbConnection.query(sql, (err, res) => {
-  if (err) throw err;
-  console.log("Character has been created with result: ", res);
-});
+const runDbBuild = cb => {
+  dbConnection.query(sql, cb);
+};
+
+module.exports = runDbBuild;
