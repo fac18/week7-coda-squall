02a03f9ac0feb4946e10e76dc68a79fdbc557731
Gillian <gillian@yeomans.org.uk>
2019-12-05 16:01:09 +0000

    start main js
    Co-authored-by: Renata <renatajarmova@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
-hi;
+const backendCall = (url, method) => {
+  let apiResponse;
+  const xml = new XMLHttpRequest();
+
+  xml.onreadystatechange = () => {
+    if (xml.readyState === 4 && xml.status === 200) {
+      apiResponse = JSON.parse(xml.responseText);
+    }
+  };
+  xml.open(method, url, true);
+  xml.send();
+  return apiResponse;
+};
+
+// .addeventlistener('load',populateAllChar)
+
+// const populateAllChar = () => {
+//     let allCharData = backendCall('/get-all-char',"GET")
+// allCharTable.textContent = allCharData
+// }
