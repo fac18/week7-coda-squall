dad8535edc9c221e4fd6dd7f87493baf0d266199
renatajarmova <renatajarmova@gmail.com>
2019-12-05 09:47:17 +0000

    Insert dummy powers into powers table
    Co-authored-by: Dan <dangoss@posteo.net>
    Co-authored-by: Roshan <roshlarosh@gmail.com>
    Co-authored-by: Gillian <gillian@yeomans.org.uk>

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -18,6 +18,12 @@ CREATE TABLE powers (
 );
 
 INSERT INTO powers (name, description, image) VALUES 
-('Electricity', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB))
+('Electricity', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Radiation', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Punch', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Clairvoyant', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Telekinesis', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Shape shifting', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB)),
+('Time manipulation', 'Description here...', (SELECT * FROM OPENROWSET(BULK N'C:\img\favicon.png', SINGLE_BLOB));
 
 COMMIT;
