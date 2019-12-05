892ab37ade033ab459e26cae6a39627cade2f5d3
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-05 11:40:48 +0000

    create postChar

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,1 +1,14 @@
-hi;
+const dbConnection = require("../database/db_connection");
+
+const postChar = (name, powers_id, talisman, battle_cry, cb) => {
+  dbConnection.query(
+    "INSERT INTO characters (name, powers_id, talisman, battle_cry) VALUES ($1, $2, $3, $4)",
+    [name, powers_id, talisman, battle_cry],
+    (err, result) => {
+      if (err) return cb(err);
+      cb(null, result);
+    }
+  );
+};
+
+module.exports = postChar;
