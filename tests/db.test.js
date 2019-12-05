4212f241954a3b29edff83340e5a5566762f885a
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-05 12:17:24 +0000

    add real database test

--- tests/db.test.js
+++ tests/db.test.js
@@ -13,11 +13,53 @@ tape("Get all powers", t => {
     if (err) console.log(err);
     return;
   });
-  let actual = getData.getPow((err, res) => {
+  let expected = [
+    {
+      id: 1,
+      name: "Electricity",
+      description: "Description here...",
+      image_path: "electricity.png"
+    },
+    {
+      id: 2,
+      name: "Radiation",
+      description: "Description here...",
+      image_path: "radiation.png"
+    },
+    {
+      id: 3,
+      name: "Punch",
+      description: "Description here...",
+      image_path: "punch.png"
+    },
+    {
+      id: 4,
+      name: "Clairvoyant",
+      description: "Description here...",
+      image_path: "clairvoyant.png"
+    },
+    {
+      id: 5,
+      name: "Telekinesis",
+      description: "Description here...",
+      image_path: "telekinesis.png"
+    },
+    {
+      id: 6,
+      name: "Shape shifting",
+      description: "Description here...",
+      image_path: "shape-shifting.png"
+    },
+    {
+      id: 7,
+      name: "Time manipulation",
+      description: "Description here...",
+      image_path: "time-manipulation.png"
+    }
+  ];
+  getData.getPow((err, res) => {
     if (err) return err;
-    return res;
+    t.deepEqual(res, expected, "These are all the powers : ");
+    t.end();
   });
-  let expected = "";
-  t.deepEqual(actual, expected, "These are all the powers : ");
-  t.end();
 });
