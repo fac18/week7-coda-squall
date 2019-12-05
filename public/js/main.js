5d5679b7599169bd8e2fda3fcfc1b888fc5e14bb
Gillian <gillian@yeomans.org.uk>
2019-12-05 17:48:33 +0000

    rig up leaderboard to dom
    Co-authored-by: Renata <renatajarmova@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -5,5 +5,29 @@ window.onload = () => {
 };
 
 const populateAllChar = res => {
-  console.log(res);
+  let allCharData = res;
+  let tableBody = document.getElementById("table-body");
+  allCharData.map(character => {
+    let row = document.createElement("tr");
+    let nameCell = document.createElement("td");
+    let talismanCell = document.createElement("td");
+    let battleCryCell = document.createElement("td");
+    let powerCell = document.createElement("td");
+    let powerImageCell = document.createElement("td");
+    let powerImage = document.createElement("img");
+
+    nameCell.textContent = character.name;
+    talismanCell.textContent = character.talisman;
+    battleCryCell.textContent = character.battle_cry;
+    powerCell.textContent = character.powers_name;
+    powerImage.src = character.image_path;
+
+    tableBody.appendChild(row);
+    row.appendChild(nameCell);
+    row.appendChild(talismanCell);
+    row.appendChild(battleCryCell);
+    row.appendChild(powerCell);
+    row.append(powerImageCell);
+    powerImageCell.append(powerImage);
+  });
 };
